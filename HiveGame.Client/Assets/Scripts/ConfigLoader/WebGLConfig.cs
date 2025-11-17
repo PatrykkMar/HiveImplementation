db1872ed37c061c76282c0b538b8d9006a21094c
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2025-11-17 22:03:15 +0100

    Added phone support

--- HiveGame.Client/Assets/Scripts/ConfigLoader/WebGLConfig.cs
+++ HiveGame.Client/Assets/Scripts/ConfigLoader/WebGLConfig.cs
@@ -11,10 +11,10 @@ public class WebGlConfig
         get
         {
             var dic = new Dictionary<string, string>();
-            dic.Add("HttpServiceUrl", "http://localhost:7200/token/token");
-            dic.Add("MatchmakingHubUrl", "http://localhost:7200/matchmakinghub");
-            //dic.Add("HttpServiceUrl", "https://www.hivegame.somee.com/token/token");
-            //dic.Add("MatchmakingHubUrl", "https://www.hivegame.somee.com/matchmakinghub");
+            //dic.Add("HttpServiceUrl", "http://localhost:7200/token/token");
+            //dic.Add("MatchmakingHubUrl", "http://localhost:7200/matchmakinghub");
+            dic.Add("HttpServiceUrl", "https://patriciothelion.bsite.net/token/token");
+            dic.Add("MatchmakingHubUrl", "https://patriciothelion.bsite.net/matchmakinghub");
             return dic;
         }
     }
--- HiveGame.Client/Assets/Scripts/UI/CameraMovement.cs
+++ HiveGame.Client/Assets/Scripts/UI/CameraMovement.cs
@@ -32,6 +32,14 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        HandleMouseControls();
+        HandleTouchControls();
+        UpdateCameraPosition();
+    }
+
+    private void HandleMouseControls()
+    {
+        // PC
         if (Input.GetMouseButton(1))
         {
             rotationX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
@@ -47,8 +55,68 @@ public class CameraMovement : MonoBehaviour
         Vector3 move = (transform.right * moveDirection.x + transform.forward * moveDirection.z) * wasdMoveSpeed * Time.deltaTime;
         move.y = 0;
         target.position += move;
+    }
 
-        UpdateCameraPosition();
+    private Vector2 lastTouchPos1;
+    private Vector2 lastTouchPos2;
+    private bool wasPinching = false;
+
+    private void HandleTouchControls()
+    {
+        // Phone
+        if (Input.touchCount == 1)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Moved)
+            {
+                rotationX += t.deltaPosition.x * sensitivity * 0.01f;
+                rotationY -= t.deltaPosition.y * sensitivity * 0.01f;
+                rotationY = Mathf.Clamp(rotationY, minAngle, maxAngle);
+            }
+
+            wasPinching = false;
+        }
+        else if (Input.touchCount == 2)
+        {
+            Touch t1 = Input.GetTouch(0);
+            Touch t2 = Input.GetTouch(1);
+
+            Vector2 pos1 = t1.position;
+            Vector2 pos2 = t2.position;
+
+            if (!wasPinching)
+            {
+                lastTouchPos1 = pos1;
+                lastTouchPos2 = pos2;
+                wasPinching = true;
+            }
+            else
+            {
+                float prevDist = (lastTouchPos1 - lastTouchPos2).magnitude;
+                float currDist = (pos1 - pos2).magnitude;
+                float diff = currDist - prevDist;
+
+                distance -= diff * 0.01f * scrollSensitivity;
+                distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+                Vector2 avgPrev = (lastTouchPos1 + lastTouchPos2) * 0.5f;
+                Vector2 avgCurr = (pos1 + pos2) * 0.5f;
+
+                Vector2 delta = avgCurr - avgPrev;
+
+                Vector3 move =
+                    (transform.right * delta.x +
+                    transform.forward * delta.y) *
+                    (wasdMoveSpeed * 0.005f);
+
+                move.y = 0;
+                target.position -= move;
+            }
+
+            lastTouchPos1 = pos1;
+            lastTouchPos2 = pos2;
+        }
     }
 
     private void UpdateCameraPosition()
